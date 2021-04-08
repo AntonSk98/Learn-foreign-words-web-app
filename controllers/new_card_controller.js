61f8c6e70d4b2378f4ec3514988fa2939fe0988e
Anton Skripin <antonskripin@gmail.com>
2021-04-08 22:57:17 +0200

    "New card" logic is slightly refactored + implemented new controller

--- controllers/new_card_controller.js
+++ controllers/new_card_controller.js
@@ -1,8 +1,55 @@
 const WordRow = require('../models/word_row')
+const Card = require('../models/card')
+
+const allCardsData = require('./all_cards_controller')
 
 const getAddNewCardPage = (req, res, next) => {
-    const wordRow1 = new WordRow('a', 'b', 'c');
-    res.render('new_card')
+    const path = 'add_new_card';
+    res.render('new_card', {
+        path: path
+    })
+}
+
+const addNewCard = (req, res, next) => {
+    allCardsData.allCards.push(parseInputToCard(req.body))
+}
+
+const parseInputToCard = inputData => {
+    inputData = convertInputDataToArray(inputData);
+    if (inputData.title === '' || inputData.description === '' || isSectiondEmpty(inputData.word) || isSectiondEmpty(inputData.translation)) {
+        // TODO you should add some validation here?
+        console.log('THERE ARE EMPTY FIELDS!')
+        return;
+    } // TODO replace if with switch case and make a separate function for the validation...
+    console.log('all is good')
+
+
+    const title = inputData.title;
+    const description = inputData.description;
+    const card = new Card(title, description);
+    inputData.word.forEach((word, index) => {
+        card.stickers = new WordRow(
+            word, inputData.translation[index], inputData.example[index]
+        );
+    });
+    console.log(card.toString())
+    return card;
+}
+
+const isSectiondEmpty = (section) => {
+    return section.some(element => element === '')
+}
+
+const convertInputDataToArray = inputData => {
+    if (!Array.isArray(inputData.word))
+        inputData.word = [inputData.word]
+    if (!Array.isArray(inputData.translation))
+        inputData.translation = [inputData.translation]
+    if (!Array.isArray(inputData.example))
+        inputData.example = [inputData.example]
+
+    return inputData;
 }
 
 exports.getAddNewCardPage = getAddNewCardPage;
+exports.addNewCard = addNewCard;
--- routes/new_card.js
+++ routes/new_card.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const newCardController = require('../controllers/new_card_controller');
 
-router.get('/add_new_card', newCardController.getAddNewCardPage)
+router.get('/add_new_card', newCardController.getAddNewCardPage);
+
+router.post('/add_new_card', newCardController.addNewCard);
 
 exports.router = router;
