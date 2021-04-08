9cf873985d66cf57dea77f64a9e6e245f1e3dc9f
Anton Skripin <antonskripin@gmail.com>
2021-04-08 22:53:20 +0200

    cards_script.js is renamed

--- public/scripts/cards_script.js
+++ public/scripts/cards_script.js
@@ -1,9 +1,17 @@
 const changeColor = () => {
     const newRow = `
-        <tr>
+        <tr class="sticker">
             <td><input type="text" class="add-new-card__sticker__input add-new-card__custom__input" name="word" placeholder="Type here a word"></td>
             <td><input type="text" class="add-new-card__sticker__input add-new-card__custom__input" name="translation" placeholder="Here goes the translation"></td>
             <td class="add-new-card__sticker__example"><input type="text" class="add-new-card__sticker__input add-new-card__custom__input" name="example" placeholder="Provide an example"></td>
         </tr>`
-    const element1 = document.getElementById("sticker").innerHTML += newRow;
+    const element = document.getElementsByClassName("sticker");
+    const lastTrElementIndex = element.length - 1;
+    element[lastTrElementIndex].insertAdjacentHTML('afterend', newRow);
+}
+
+const hideShowTooltip = (value) => {
+    const element = document.getElementsByClassName("tooltip")[value];
+    const elementVisibility = window.getComputedStyle(element).visibility;
+    elementVisibility === 'visible' ? element.style.visibility = 'hidden' : element.style.visibility = 'visible';
 }
