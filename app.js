ab6c4376e5c5023a0484938eb2bbdfae7b226b82
Anton Skripin <antonskripin@gmail.com>
2021-04-08 22:52:35 +0200

    added Card routes; added urlencoded to get data from input fields

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const path = require('path')
 
 const app = express();
 
-const newCardRouter = require('./routes/new_card')
+const newCardRouter = require('./routes/new_card');
+const allCardsRouter = require('./routes/all_cards');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({extended: true}))
 app.use('', newCardRouter.router);
+app.use('', allCardsRouter.router);
 
 app.listen(8000, () => {
     console.log('Application is started successfully!')
