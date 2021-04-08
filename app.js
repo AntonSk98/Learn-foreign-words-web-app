2dd27b089fdb0aa7b3dbfb6dc562c22a9503f1f6
Anton Skripin <antonskripin@gmail.com>
2021-04-08 22:57:17 +0200

    small fixes and image added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 const newCardRouter = require('./routes/new_card');
 const allCardsRouter = require('./routes/all_cards');
 
+const errorController = require('./controllers/error_controller')
+
 app.set('view engine', 'ejs');
 app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +15,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 app.use('', newCardRouter.router);
 app.use('', allCardsRouter.router);
+
+app.use(errorController.get404Page);
 
 app.listen(8000, () => {
     console.log('Application is started successfully!')
